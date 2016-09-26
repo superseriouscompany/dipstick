1f415b2cba4639db8658f8610f967b4e20815a0d
Neil Sarkar <neil.sarkar@gotinder.com>
2016-09-26 17:01:56 +0100

    Provide hello world app with a single ping endpoint

---
+++ README.md
@@ -0,0 +1,7 @@
+# dipstick
+
+A nodejs app to test operational configurations.
+
+## Running
+
+    $ npm run dev
---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bunyan  = require('bunyan');
+
+var app     = express();
+var log     = bunyan.createLogger({ name: 'dipstick' });
+var port    = process.env.PORT || 3000;
+
+log.info('hi');
+
+app.get('*', function(req, res) {
+  res.json({hi: true})
+})
+
+app.listen(port, function() {
+  log.info({port: port}, 'server is running');
+})
+
+process.on('uncaughtException', function(err) {
+  log.error({err: err}, 'Uncaught Error');
+  throw err;
+})
